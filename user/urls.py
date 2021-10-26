5881bd9f16e7066925ab905fc1ebc2a650c7eed0
Андрей Голубев <kpdpav@gmail.com>
2021-10-26 06:08:50 +0300

    add(user): added the ability to get a user from a request.

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from rest_framework_jwt.views import obtain_jwt_token
 
+from .views import UserRetrieveAPIView
+
 
 urlpatterns = [
     path('token-create/', obtain_jwt_token, name='obtain_jwt_token'),
+    path('profile/', UserRetrieveAPIView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    RetrieveAPIView
+)
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import User
+
+from .serializer import UserSerializer
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.get_serializer(instance=request.user)
+        return Response(serializer.data)
