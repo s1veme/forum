886a9e6e89ffdb88e7790ff177bb13f4d7c449b9
Viacheslav <vapoltavecs@gmail.com>
2021-12-28 15:45:22 +0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log('aaa');
+const aaa = 1;
+console.log('bbbb');
+
+
+
+console.log('1');
+console.log('ура')
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+let path = require('path');
+
+let conf = {
+
+    entry: './src/main.js',
+
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    devServer: {
+        static: __dirname + '/',
+        devMiddleware: {
+            publicPath: '/dist/',
+        },
+        
+        compress: true,
+        client: {
+            overlay: true,
+        },
+
+
+    },
+};
+
+module.exports = (env, argv) => {
+    conf.devtool = (argv.mode === 'production') ? 'hidden-source-map' : 'eval-cheap-module-source-map';
+    return conf;
+}
