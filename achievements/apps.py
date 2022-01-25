bda405da39db3e27de52921f36f3fa94f5d1c010
s1veme <kpdpav@gmail.com>
2022-01-25 18:38:26 +0300

    add(achievements): add request objects Answer for user.

--- achievements/apps.py
+++ achievements/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AchievementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'achievements'
+
+    def ready(self):
+        import achievements.signals
--- achievements/signals.py
+++ achievements/signals.py
@@ -1,9 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from user.models import User
+
+from posts.models import Answer
 
 
-@receiver(post_save, sender=User)
-def check_achievement(sender, **kwargs):
+@receiver(post_save, sender=Answer)
+def check_achievement(sender, instance, **kwargs):
+    number_answers = Answer.objects.filter(owner=instance.owner).count()
     # todo: checking if the user deserved the achievement and issuing achievements
-    ...
